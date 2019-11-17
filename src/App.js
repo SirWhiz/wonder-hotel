27dfea22ba7853709a2e084d107d07c4decbf49f
Alvaro <applewhiz@outlook.es>
2019-11-17 10:54:00 +0100

    Updated App Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header';
+import bedroom from './assets/bedroom.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-pic">
+        <Header/>
+        <div className="call-to-action">
+          <div className="cta-title">The Wonder Resort</div>
+        </div>
+      </div>
+      <div className="about">
+        <div className="about-row">
+          <div>A Luxurious place in the heart of America</div>
+          <div className="bedroom-img"><img src={bedroom} /></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
