58cf0ec65bd054d839c158db056402b521a87aec
Alvaro Ferrera <applewhiz@outlook.es>
2019-11-18 23:34:49 +0100

    Finished about section and moved it to a different component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
+
 import Header from './components/header/header';
-import bedroom from './assets/bedroom.jpg';
-import bedroomBed from './assets/bedroom-bed.jpg';
+import About from './components/about/About';
 
 function App() {
   return (
@@ -13,24 +13,7 @@ function App() {
           <div className="cta-title">The Wonder Resort</div>
         </div>
       </div>
-      <div className="about">
-        <div className="about-row">
-          <div className="about-text">
-            <span className="section-flag">About Us</span>
-            <span className="big-title">A Luxurious place in the heart of America</span>
-            <div className="big-text-about">
-              Suscipit libero pretium nullam potenti. Interdum, blandit phasellus consectetuer dolor ornare dapibus
-              enim ut tincidunt rhoncus tellus sollicitudin pede nam maecenas, dolor sem. Neque sollicitudin enim. 
-              Dapibus lorem feugiat facilisi faucibus et. Rhoncus.
-            </div>
-          </div>
-          <div className="about-pics">
-            <div className="bedroom-img"><img src={bedroom} alt="bedroom" /></div>
-            <div className="bedroom-img"><img src={bedroomBed} alt="bedroom bed"/></div>
-          </div>
-          <div></div>
-        </div>
-      </div>
+      <About/>
     </div>
   );
 }
---
+++ src/components/about/About.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './About.css';
+
+import bedroom from '../../assets/bedroom.jpg';
+import bedroomBed from '../../assets/bedroom-bed.jpg';
+
+function About() {
+    return(
+        <div className="about">
+            <div className="about-row">
+                <div className="about-text">
+                    <span className="section-flag">About Us</span>
+                    <span className="big-title">A Luxurious place in the heart of America</span>
+                    <div className="big-text-about">
+                    Suscipit libero pretium nullam potenti. Interdum, blandit phasellus consectetuer dolor ornare dapibus
+                    enim ut tincidunt rhoncus tellus sollicitudin pede nam maecenas, dolor sem. Neque sollicitudin enim. 
+                    Dapibus lorem feugiat facilisi faucibus et. Rhoncus.
+                    </div>
+                </div>
+                <div className="about-pics">
+                    <div className="bedroom-img"><img src={bedroom} alt="bedroom" /></div>
+                    <div className="bedroom-img"><img src={bedroomBed} alt="bedroom bed"/></div>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
