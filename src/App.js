c210bcdf30ee0fdade94597268b9bad34264a51c
Alvaro <applewhiz@outlook.es>
2019-11-18 19:06:42 +0100

    Finished About section

--- src/App.js
+++ src/App.js
@@ -18,6 +18,11 @@ function App() {
           <div className="about-text">
             <span className="section-flag">About Us</span>
             <span className="big-title">A Luxurious place in the heart of America</span>
+            <div className="big-text-about">
+              Suscipit libero pretium nullam potenti. Interdum, blandit phasellus consectetuer dolor ornare dapibus
+              enim ut tincidunt rhoncus tellus sollicitudin pede nam maecenas, dolor sem. Neque sollicitudin enim. 
+              Dapibus lorem feugiat facilisi faucibus et. Rhoncus.
+            </div>
           </div>
           <div className="about-pics">
             <div className="bedroom-img"><img src={bedroom} alt="bedroom" /></div>
