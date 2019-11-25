fc3a45fb9bf8809fe8a841591afdd9023f802c21
Alvaro Ferrera <applewhiz@outlook.es>
2019-11-25 23:42:43 +0100

    Added sticky navigation

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './header.css';
 
-function Header() {
-    
-    return (
-        <div className="header">
-            <header>
-                <div className="header-menu">
-                    <span>Home</span>
-                    <span>Rooms</span>
-                    <span>About</span>
-                    <span>Contact</span>
-                </div>
-                <div className="logo">LOGO</div>
-                <div className="book-button">
-                    <button>Book Your Room</button>
-                </div>
-                <div className="mobile-button">
-                    <i className="fas fa-bars fa-2x clickable"></i>
-                </div>
-            </header>
-        </div>
-    );
+class Header extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            scrolled: false
+        };
+    }
+
+    componentDidMount(){
+        window.addEventListener('scroll', () => {
+            const isTop = window.scrollY < 180;
+            if(isTop !== true){
+                this.setState({ scrolled: true });
+            }else{
+                this.setState({ scrolled: false });
+            }
+        });
+    }
+
+    render(){
+        return (
+            <div className={this.state.scrolled ? 'header scrolled' : 'header'}>
+                <header>
+                    <div className="header-menu">
+                        <span>Home</span>
+                        <span>Rooms</span>
+                        <span>About</span>
+                        <span>Contact</span>
+                    </div>
+                    <div className="logo">LOGO</div>
+                    <div className="book-button">
+                        <button>Book Your Room</button>
+                    </div>
+                    <div className="mobile-button">
+                        <i className="fas fa-bars fa-2x clickable"></i>
+                    </div>
+                </header>
+            </div>
+        );
+    }
+
 }
 
 export default Header;
