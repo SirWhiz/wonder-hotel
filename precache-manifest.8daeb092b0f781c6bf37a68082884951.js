6269171cb64d8d4d0bbbcf6cbe7643370d802a7e
Alvaro <applewhiz@outlook.es>
2020-01-26 17:44:59 +0100

    Updates

---
+++ precache-manifest.8daeb092b0f781c6bf37a68082884951.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec71481d5c66f4f77421e7c6937793c",
+    "url": "/wonder-hotel/index.html"
+  },
+  {
+    "revision": "76663c43e5306c2824e5",
+    "url": "/wonder-hotel/static/css/2.cd67af34.chunk.css"
+  },
+  {
+    "revision": "c6772f7d11036f00766d",
+    "url": "/wonder-hotel/static/css/main.b7114db6.chunk.css"
+  },
+  {
+    "revision": "76663c43e5306c2824e5",
+    "url": "/wonder-hotel/static/js/2.c2efad27.chunk.js"
+  },
+  {
+    "revision": "c6772f7d11036f00766d",
+    "url": "/wonder-hotel/static/js/main.7881cd0a.chunk.js"
+  },
+  {
+    "revision": "8c165dccbc08aceed346",
+    "url": "/wonder-hotel/static/js/runtime-main.f24396e8.js"
+  },
+  {
+    "revision": "2cc279aafb773db8dbad14703d6c786d",
+    "url": "/wonder-hotel/static/media/bedroom-bed.2cc279aa.jpg"
+  },
+  {
+    "revision": "8389df6a418f8c43cae6eccb40cf2d47",
+    "url": "/wonder-hotel/static/media/bedroom.8389df6a.jpg"
+  },
+  {
+    "revision": "8324a6142c5fc9d0bdd0b453c162f9a1",
+    "url": "/wonder-hotel/static/media/fish.8324a614.jpg"
+  },
+  {
+    "revision": "08e04e6a0e357cee66638bd03cbd260a",
+    "url": "/wonder-hotel/static/media/hotel-main.08e04e6a.jpg"
+  },
+  {
+    "revision": "77abf62e73a098cf7753e02292525168",
+    "url": "/wonder-hotel/static/media/master-card.77abf62e.jpg"
+  },
+  {
+    "revision": "3406cd9c9dd06d8ce2eca63d85726107",
+    "url": "/wonder-hotel/static/media/meat.3406cd9c.jpg"
+  },
+  {
+    "revision": "aea323e82a7a5415b5c04de09a1da56e",
+    "url": "/wonder-hotel/static/media/members.aea323e8.jpg"
+  },
+  {
+    "revision": "ad63f78464fd8a42d4d44535250c2104",
+    "url": "/wonder-hotel/static/media/pool.ad63f784.jpg"
+  },
+  {
+    "revision": "489d0ab6b6177a84124364437a743555",
+    "url": "/wonder-hotel/static/media/weekend.489d0ab6.jpg"
+  },
+  {
+    "revision": "650253c582c180d041a413a86a652154",
+    "url": "/wonder-hotel/static/media/wine.650253c5.jpg"
+  },
+  {
+    "revision": "b649ab51805b6b47e1091bc115861c68",
+    "url": "/wonder-hotel/static/media/woman-bed.b649ab51.jpg"
+  }
+]);
