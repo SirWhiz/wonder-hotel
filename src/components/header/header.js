489d3311b1edccaf8677485e88b3ecf0e1b7a481
Alvaro <applewhiz@outlook.es>
2020-01-26 16:14:26 +0100

    Added smooth scrolling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import { Link } from "react-scroll";
 import './header.css';
 
 class Header extends Component {
@@ -30,12 +32,14 @@ class Header extends Component {
             <div className={this.state.scrolled ? 'header scrolled' : 'header'}>
                 <header>
                     <div className="header-menu">
-                        <span>Home</span>
-                        <span>Rooms</span>
-                        <span>About</span>
-                        <span>Contact</span>
+                        <Link className="test" to="home" smooth={true} offset={-70}>Home</Link>
+                        <Link className="test" to="about" smooth={true}>About</Link>
+                        <Link className="test" to="food" smooth={true}>Food</Link>
+                        <Link className="test" to="contact" smooth={true} offset={-100}>Contact</Link>
                     </div>
-                    <div className="logo">LOGO</div>
+                    <div className="logo">
+                        <i className={this.state.scrolled ? 'fas fa-hotel fa-2x main-color' : 'fas fa-hotel fa-2x logo-color'}></i>
+                    </div>
                     <div className="book-button">
                         <button>Book Your Room</button>
                     </div>
